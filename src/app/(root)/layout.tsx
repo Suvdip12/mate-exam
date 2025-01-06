09624c09009c9bb15d144c7bb7fa23e24c64bac2
CoderNandan <nandanindia1947@gmail.com>
2025-01-06 17:19:31 +0530

    nav bar remove

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Navbar from "@/components/Navbar";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Navbar />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
