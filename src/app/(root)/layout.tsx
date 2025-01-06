36c8dfd5cd8d56cb765558919110b201e2787680
CoderNandan <nandanindia1947@gmail.com>
2025-01-06 17:22:30 +0530

    add Navbar component to Layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "@/components/Navbar";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
